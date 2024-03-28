955e0bf96fa2f2cedd60dd3212ca0cfc1b0d0be6
chenxueli <zjphchenxueli@163.com>
2024-03-28 15:48:34 +0800

    合并项目信息

--- packages/basic-base-business/build.config.ts
+++ packages/basic-base-business/build.config.ts
@@ -1,11 +1,16 @@
 import { defineBuildConfig } from 'unbuild'
 
 export default defineBuildConfig({
+  clean: true,
+  declaration: true,
   entries: [
     './src/index',
   ],
-  declaration: true,
-  clean: true,
+  externals: [
+    '@own-basic-component/config',
+    '@own-basic-component/request',
+    '@own-basic-component/util',
+  ],
   rollup: {
     emitCJS: true,
   },
