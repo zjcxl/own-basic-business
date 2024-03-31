d42765c3b91b3e2f9be3f51a0c0bcd682891816b
chenxueli <zjphchenxueli@gmail.com>
2024-03-31 16:08:35 +0800

    添加管理员菜单信息的分页查询

--- packages/basic-base-business/src/menu/api/index.ts
+++ packages/basic-base-business/src/menu/api/index.ts
@@ -1,5 +1,7 @@
 import apiBusinessSystemMenuUser from './system-menu-user-api'
+import apiBusinessSystemMenuManage from './system-menu-manage-api'
 
 export {
   apiBusinessSystemMenuUser,
+  apiBusinessSystemMenuManage,
 }
