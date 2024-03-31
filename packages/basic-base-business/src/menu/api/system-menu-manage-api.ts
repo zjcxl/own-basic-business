a9ca2f1c7af81ededa810b9c0c3fc62ac957dbbb
chenxueli <zjphchenxueli@163.com>
2024-03-31 16:49:12 +0800

    添加菜单的接口

--- packages/basic-base-business/src/menu/api/system-menu-manage-api.ts
+++ packages/basic-base-business/src/menu/api/system-menu-manage-api.ts
@@ -1,6 +1,6 @@
-import { PostRequestModel } from '@own-basic-component/request'
+import { DeleteRequestModel, GetRequestModel, PostRequestModel, PutRequestModel } from '@own-basic-component/request'
 import type { PageResultModel } from '@own-basic-component/config'
-import type { SystemMenuVo } from '../entity'
+import type { SystemMenuDto, SystemMenuVo } from '../entity'
 import type { SystemMenuQuery } from '../entity/system-menu-query'
 
 /**
@@ -15,6 +15,35 @@ function page(query: Partial<SystemMenuQuery>) {
   return new PostRequestModel<PageResultModel<SystemMenuVo>>(`${prefix}/page`, query).request()
 }
 
+/**
+ * 根据id获取信息
+ * @param id 主键id
+ */
+const getById = (id: string) => new GetRequestModel<SystemMenuVo>(`${prefix}/${id}`).request()
+
+/**
+ * 根据id删除信息
+ * @param id 主键id
+ */
+const deleteById = (id: string) => new DeleteRequestModel<SystemMenuVo>(`${prefix}/${id}`).request()
+
+/**
+ * 新增
+ * @param form 表单信息
+ */
+const add = (form: SystemMenuDto) => new PostRequestModel<SystemMenuVo>(`${prefix}/`, form).request()
+
+/**
+ * 修改
+ * @param id 主键id
+ * @param form 表单信息
+ */
+const updateById = (id: string, form: SystemMenuDto) => new PutRequestModel<SystemMenuVo>(`${prefix}/${id}`, form).request()
+
 export default {
   page,
+  add,
+  getById,
+  updateById,
+  deleteById,
 }
