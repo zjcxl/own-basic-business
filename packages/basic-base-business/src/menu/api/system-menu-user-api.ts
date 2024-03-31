fb07d07a90746c0a911c3c64297f9ebcbd4c1191
chenxueli <zjphchenxueli@163.com>
2024-03-31 22:34:54 +0800

    添加查询当前用户所有可用的菜单信息

--- packages/basic-base-business/src/menu/api/system-menu-user-api.ts
+++ packages/basic-base-business/src/menu/api/system-menu-user-api.ts
@@ -8,6 +8,13 @@ import type { SystemMenuMapVo } from '../entity'
 const prefix = 'u/system/menu'
 
 /**
+ * 查询当前用户所有可用的菜单信息
+ */
+function all() {
+  return new GetRequestModel<Array<SystemMenuMapVo>>(`${prefix}/all`).request()
+}
+
+/**
  * 查询当前用户的菜单树信息
  */
 function tree() {
@@ -15,5 +22,6 @@ function tree() {
 }
 
 export default {
+  all,
   tree,
 }
