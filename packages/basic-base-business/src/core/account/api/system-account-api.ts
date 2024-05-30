b6e98f27225c463e254f8812cc18be7e334c39ef
chenxueli <zjphchenxueli@163.com>
2024-05-30 14:07:16 +0800

    add SystemAccountQuery

--- packages/basic-base-business/src/core/account/api/system-account-api.ts
+++ packages/basic-base-business/src/core/account/api/system-account-api.ts
@@ -1,5 +1,6 @@
-import { PostRequestModel } from '@own-basic-component/request'
-import type { SystemAccountVo } from '../entity'
+import { DeleteRequestModel, PostRequestModel } from '@own-basic-component/request'
+import type { PageResultModel, QueryObjectType } from '@own-basic-component/config'
+import type { SystemAccountQuery, SystemAccountVo } from '../entity'
 
 /**
  * 请求前缀
@@ -7,9 +8,17 @@ import type { SystemAccountVo } from '../entity'
 const prefix = 'm/system/account'
 
 /**
- * 根据用户id查询账号列表
- * @param userId 查询条件
+ * 分页查询
+ * @param query 查询条件
  */
-export async function listByUserId(userId: string) {
-  return new PostRequestModel<Array<SystemAccountVo>>(`${prefix}/all/${userId}`).request()
+export async function page(query?: Partial<SystemAccountQuery> | QueryObjectType) {
+  return new PostRequestModel<PageResultModel<SystemAccountVo>>(`${prefix}/page`, query).request()
+}
+
+/**
+ * 根据id删除信息
+ * @param id
+ */
+export async function deleteById(id: string) {
+  return new DeleteRequestModel<boolean>(`${prefix}/${id}`).request()
 }
--- packages/basic-base-business/src/core/account/entity/system-account.ts
+++ packages/basic-base-business/src/core/account/entity/system-account.ts
@@ -1,4 +1,4 @@
-import type { AccountType, BaseVo, YesNoType } from '../../../base'
+import type { AccountType, BaseQuery, BaseVo, YesNoType } from '../../../base'
 
 export interface SystemAccountVo extends BaseVo {
   /**
@@ -13,6 +13,29 @@ export interface SystemAccountVo extends BaseVo {
    * 用户id
    */
   fkUserId: string
+  /**
+   * 是否启用 1=启用 0=禁用
+   */
+  enable: YesNoType
+  /**
+   * 账号类型 1=普通用户 2=管理员
+   */
+  accountType: AccountType
+}
+
+export interface SystemAccountQuery extends BaseQuery {
+  /**
+   * 账号
+   */
+  account: string
+  /**
+   * 类型
+   */
+  type: string
+  /**
+   * 用户id
+   */
+  userId: string
   /**
    * 是否启用 1=启用 0=禁用
    */
