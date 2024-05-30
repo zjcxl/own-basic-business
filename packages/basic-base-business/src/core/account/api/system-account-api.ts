25753214554d90432e34e601ecb9d219e557b2b5
chenxueli <zjphchenxueli@163.com>
2024-05-30 14:40:24 +0800

    fix SystemAccount

--- packages/basic-base-business/src/core/account/api/system-account-api.ts
+++ packages/basic-base-business/src/core/account/api/system-account-api.ts
@@ -1,11 +1,20 @@
-import { DeleteRequestModel, PostRequestModel } from '@own-basic-component/request'
+import { DeleteRequestModel, PostRequestModel, PutRequestModel } from '@own-basic-component/request'
 import type { PageResultModel, QueryObjectType } from '@own-basic-component/config'
 import type { SystemAccountQuery, SystemAccountVo } from '../entity'
+import type { YesNoType } from '../../../base'
 
 /**
  * 请求前缀
  */
 const prefix = 'm/system/account'
+
+/**
+ * 分页查询
+ * @param query 查询条件
+ */
+export async function all(query?: Partial<SystemAccountQuery> | QueryObjectType) {
+  return new PostRequestModel<Array<SystemAccountVo>>(`${prefix}/all`, query).request()
+}
 
 /**
  * 分页查询
@@ -21,4 +30,13 @@ export async function page(query?: Partial<SystemAccountQuery> | QueryObjectType
  */
 export async function deleteById(id: string) {
   return new DeleteRequestModel<boolean>(`${prefix}/${id}`).request()
+}
+
+/**
+ * 根据id修改启用状态
+ * @param id
+ * @param enable
+ */
+export async function updateEnableById(id: string, enable: YesNoType) {
+  return new PutRequestModel<boolean>(`${prefix}/enable/${id}/${enable}`).request()
 }
